a9c1f2e88b05c1e6e7665120a24fe0bfe99ebd53
Marcus Koosmann <koosmann@gmail.com>
2020-07-04 16:45:01 -0700

    Removed tests

--- sketches/test-2/test.js
+++
@@ -1,9 +0,0 @@
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-}
-
-function draw() {
-  background(220);
-  fill('red');
-  ellipse(50,50,80,80);
-}
--- sketches/test/test.js
+++
@@ -1,8 +0,0 @@
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-}
-
-function draw() {
-  background(220);
-  ellipse(50,50,80,80);
-}
